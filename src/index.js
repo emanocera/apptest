4596c0c3cc126ba8402f6cb25f0333ea1b63fd28
emanocera <ema@contabilium.com>
2021-02-12 12:21:55 -0300

    build settings

---
+++ src/index.js
@@ -0,0 +1,30 @@
+//src/index.js
+
+import * as styles from './styles';
+import * as fonts from './fonts';
+import * as images from './images';
+
+export { styles, fonts, images };
+
+export * from './components/AddItem';
+export * from './components/Badge';
+export * from './components/Button';
+export * from './components/Dropdown';
+export * from './components/Favorites';
+export * from './components/HelpButtons';
+export * from './components/Icon';
+export * from './components/IconButton';
+export * from './components/MainMenu';
+export * from './components/MainMenu/MainMenuItem';
+export * from './components/Notifications';
+export * from './components/PageBar';
+export * from './components/PageFooter';
+export * from './components/Search';
+export * from './components/Sidebar';
+export * from './components/SidebarUser';
+export * from './components/Tabs';
+export * from './components/Tabs/Tab';
+export * from './components/Tabs/TabPanel';
+export * from './components/TextField';
+export * from './components/Tooltip';
+export * from './components/TopNavbar';
